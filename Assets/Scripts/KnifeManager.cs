01b6b6f668913b5c5c7d5a41b5624bd62c11986f
salihkarakus <sskarakussalih77@gmail.com>
2025-02-18 23:46:38 +0300

    knife manager update

--- Assets/Scripts/KnifeManager.cs
+++ Assets/Scripts/KnifeManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class KnifeManager : MonoBehaviour
 {
-  
+  [SerializeField] private List<GameObject> knifeList = new List<GameObject>();
+  [SerializeField] private GameObject knifePrefab;
+  [SerializeField] private int knifeCount;
+  private int knifeIndex;
+
     void Start()
     {
-        
+        CreateKnife();
     }
 
-    
-    void Update()
+    private void CreateKnife()
     {
-        
+        for(int i=0; i < knifeCount; i++)
+        {
+          GameObject newKnife = Instantiate(knifePrefab , transform.position , quaternion.identity);
+          newKnife.SetActive(false);
+          knifeList.Add(newKnife);
+        }
+
+        knifeList[0].SetActive(true);
     }
+
 }
